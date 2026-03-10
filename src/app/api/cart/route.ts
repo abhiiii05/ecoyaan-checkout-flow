a281218aafd4a483ad5c533447a024ed40529757
abhiiii.iiii <abhijithviju2005@gmail.com>
2026-03-10 19:25:50 +0530

    api route (mock)

---
+++ src/app/api/cart/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import { getCartData } from "@/lib/cart";
+
+
+export async function GET() {
+  const data = await getCartData();
+  return NextResponse.json(data);
+  
+}
